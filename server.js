35d3fb3f47fb9c0d6fb7bb5c9e0b643c6e56e2e5
kucingandromeda <agvayazem@gmail.com>
2024-02-27 15:49:38 +0700

    menambahkan format api dalam betuk json yang akan menjadi response server

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-console.log("halo");
+///set up phase
+// set up express
+const express = require("express");
+const app = express();
+
+//set up file system
+const fs = require("fs");
+
+//exe phase
+app.get("/newsData", (req, res) => {
+  fs.readFile(
+    "./database/newsData.json",
+    { encoding: "utf-8" },
+    (err, data) => {
+      res.send(data);
+    }
+  );
+});
+
+app.listen(8000, () => {
+  console.log("server running");
+});
