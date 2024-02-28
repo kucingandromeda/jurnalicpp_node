b960a83a29c05d62d810c239c360b023908bb432
kucingandromeda <agvayazem@gmail.com>
2024-02-28 07:01:19 +0700

    menambahkan Acces-Control-Allow-Origin

--- server.js
+++ server.js
@@ -3,6 +3,14 @@
 const express = require("express");
 const app = express();
 
+//set up cors
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "http://localhost:5173/",
+  })
+);
+
 //set up file system
 const fs = require("fs");
 
@@ -12,6 +20,22 @@ app.get("/newsData", (req, res) => {
     "./database/newsData.json",
     { encoding: "utf-8" },
     (err, data) => {
+      res.send(data);
+    }
+  );
+});
+
+app.get("/getData/:value", (req, res, next) => {
+  const request = req.params.value;
+  fs.readFile(
+    `./database/api/${request}.json`,
+
+    (err, data) => {
+      res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+      res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+      );
       res.send(data);
     }
   );
