072d3215955a72370b2d582aeaec81a1c6c3896a
kucingandromeda <agvayazem@gmail.com>
2024-03-11 12:11:41 +0700

    sudah membuat reg untuk edit dan del juga manambahkan nama subdomain

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 ///set up phase
 
 //variable area
-// const frontEndUrl = "http://localhost:5173";
-const frontEndUrl = "https://jurnalicpp.online";
+const frontEndUrl = "http://localhost:5173";
+// const frontEndUrl = ["https://jurnalicpp.online", "https://admin2007.jurnalicpp.online"];
 
 //set up path
 const path = require("path");
@@ -21,19 +21,19 @@ const fs = require("fs");
 //setup mysql
 const mysql = require("mysql");
 
-const db = mysql.createConnection({
-  host: "103.175.216.188",
-  user: "jicpp",
-  password: "ADMIN",
-  database: "jicpp",
-});
-
 // const db = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "",
+//   host: "103.175.216.188",
+//   user: "jicpp",
+//   password: "ADMIN",
 //   database: "jicpp",
 // });
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "jicpp",
+});
 
 db.connect(function (err) {
   if (err) throw err;
@@ -111,8 +111,8 @@ app.post("/admin/publish", upload.array("files"), (req, res) => {
         const penulis = req.body.penulis;
         const genre = req.body.genre;
         const img = req.body.imgStat
-          ? null
-          : `${data.filename.replace(".json", "")}IMG.png`;
+          ? `${data.filename.replace(".json", "")}IMG.png`
+          : null;
         console.log(img);
         const url = data.filename.replace(".json", "");
         db.query(
@@ -135,6 +135,95 @@ app.post("/admin/publish", upload.array("files"), (req, res) => {
   } else {
     res.send({ status: "error", msg: "errrr" });
   }
+});
+
+//edit
+
+app.post("/admin/edit", upload.single("atribut"), (req, res) => {
+  const stat = {
+    dbMysql: false,
+    dbDir: false,
+  };
+  res.header("Access-Control-Allow-Origin", frontEndUrl);
+  if (req.body.id && req.body.atribut && req.body.new) {
+    db.query(
+      `UPDATE a_news_data SET ${req.body.atribut}='${req.body.new}' WHERE id=${req.body.id}`,
+      (err, result) => {
+        if (err) throw err;
+        if (result) {
+          stat.dbMysql = true;
+          fs.readFile(
+            `./db/api/${req.body.Bjudul}.json`,
+            { encoding: "utf-8" },
+            (err, data) => {
+              if (err) throw err;
+              const dataNews = JSON.parse(data);
+              dataNews[req.body.atribut] = req.body.new;
+
+              if (req.body.atribut === "judul") {
+                const url = dataNews.judul;
+                const newUrl = url.replace(/ /g, "-");
+                db.query(
+                  `UPDATE a_news_data SET url='${newUrl}' WHERE id=${req.body.id}`,
+                  (err, result) => {
+                    if (err) throw err;
+
+                    dataNews["url"] = newUrl;
+                    fs.rm(`./db/api/${req.body.Bjudul}.json`, (err) => {
+                      if (err) throw err;
+                      fs.writeFile(
+                        `./db/api/${newUrl}.json`,
+                        JSON.stringify(dataNews, null, 2),
+                        (err) => {
+                          if (err) throw err;
+                          console.log("berhasil diubah");
+                        }
+                      );
+                    });
+                  }
+                );
+              } else {
+                fs.writeFile(
+                  `./db/api/${req.body.Bjudul}.json`,
+                  JSON.stringify(dataNews, null, 2),
+                  (err) => {
+                    if (err) throw err;
+                    console.log("berhasil diubah");
+                  }
+                );
+              }
+            }
+          );
+        }
+      }
+    );
+    res.send({ status: "ok" });
+  } else {
+    res.send({ status: "error", m: "ada data yang tdk lengkap" });
+  }
+});
+
+app.post("/admin/del", upload.array("id"), (req, res) => {
+  const stat = {
+    dbMysql: false,
+    dbDir: false,
+  };
+  res.header("Access-Control-Allow-Origin", frontEndUrl);
+  db.query(`DELETE FROM a_news_data WHERE id=${req.body.id}`, (err, result) => {
+    if (err) throw err;
+
+    stat.dbMysql = true;
+    fs.rm(`./db/api/${req.body.Bjudul}.json`, (err) => {
+      if (err) throw err;
+
+      fs.rm(`./db/api/${req.body.Bjudul}IMG.png`, (err) => {
+        if (err) throw err;
+
+        stat.dbDir = true;
+        res.send({ status: stat });
+      });
+    });
+  });
 });
 
 app.listen(8000, () => {
